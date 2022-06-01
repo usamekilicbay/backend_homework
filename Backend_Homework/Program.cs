b3bec4814d9cea20fbd8fd4b266907127460a63d
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-06-01 14:48:55 +0300

    refactor: update var keywords as correct types

--- Backend_Homework/Program.cs
+++ Backend_Homework/Program.cs
@@ -15,10 +15,10 @@ namespace Continero.Homework
         static void Main(string[] args)
         {
             // I'm not sure about these lines yet, I'll consider them later.
-            var sourceFileName = Path.Combine(Environment.CurrentDirectory, "..\\..\\..\\Source Files\\Document1.xml");
+            string sourceFileName = Path.Combine(Environment.CurrentDirectory, "..\\..\\..\\Source Files\\Document1.xml");
 
 
-            var targetFileName = Path.Combine(Environment.CurrentDirectory, "..\\..\\..\\Target Files\\Document1.json");
+            string targetFileName = Path.Combine(Environment.CurrentDirectory, "..\\..\\..\\Target Files\\Document1.json");
 
             string fileContext = ReadFile(sourceFileName);
 
@@ -30,15 +30,15 @@ namespace Continero.Homework
 
         private static void ConvertFile(string targetFileName, string fileContext)
         {
-            var xdoc = XDocument.Parse(fileContext);
+            XDocument xdoc = XDocument.Parse(fileContext);
             // Potential issue 4: title and text nodes might not be exist in xml file.
-            var doc = new Document
+            Document doc = new()
             {
                 Title = xdoc.Root.Element("title").Value,
                 Text = xdoc.Root.Element("text").Value
             };
 
-            var serializedDoc = JsonConvert.SerializeObject(doc);
+            string serializedDoc = JsonConvert.SerializeObject(doc);
             WriteFile(targetFileName, serializedDoc);
         }
 
@@ -46,9 +46,9 @@ namespace Continero.Homework
         {
             // Potential issue 5: This part of code should be inside of try-catch blocks for avoiding possible issues while writing/creating the file.
             // Potential issue 6: targetStream and sw should closed and disposed.
-            var targetStream = File.Open(targetFileName, FileMode.Create, FileAccess.Write);
+            FileStream targetStream = File.Open(targetFileName, FileMode.Create, FileAccess.Write);
             // Potential issue 7: As long as sw is not closed or flushed, writing operation won't be completed. Application might not throw errors but writed file will be empty.
-            var sw = new StreamWriter(targetStream);
+            StreamWriter sw = new(targetStream);
             sw.Write(serializedDoc);
         }
 
@@ -60,7 +60,7 @@ namespace Continero.Homework
                 // Potential issue 1: sourceStream not closed. It might cause issues for other reading operations.
                 // Potential issue 2: Also it will effect the memory because not disposed as well. I would use "using  statement" to avoid these issues.
                 FileStream sourceStream = File.Open(sourceFileName, FileMode.Open);
-                var reader = new StreamReader(sourceStream);
+                StreamReader reader = new(sourceStream);
                 return reader.ReadToEnd();
             }
             catch (Exception ex)
