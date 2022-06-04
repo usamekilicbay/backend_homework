4a1e217dafa492dc95e499003b5d6fc0f1a4247d
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-06-04 18:40:17 +0300

    feat: add FileSystemFileWriter class and inherit interfaces, fill WriteFile method

---
+++ Backend_Homework/Concrete/FileSystemFileWriter.cs
@@ -0,0 +1,26 @@
+﻿using Backend_Homework.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend_Homework.Concrete
+{
+    internal class FileSystemFileWriter : IFileWriter
+    {
+        public void WriteFile(string targetFileName, string content)
+        {
+            try
+            {
+                using FileStream targetStream = File.Open(targetFileName, FileMode.Create, FileAccess.Write);
+                using StreamWriter streamWriter = new(targetStream);
+                streamWriter.Write(content);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An error occured while writing the file", ex);
+            }
+        }
+    }
+}
