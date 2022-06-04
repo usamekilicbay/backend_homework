c1e2376d680e5b3f0adb7141b1d66e989b304b28
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-06-04 18:42:12 +0300

    feat: add Type code file for StorageOption and ConvertOption enums

---
+++ Backend_Homework/Type.cs
@@ -0,0 +1,16 @@
+﻿namespace Backend_Homework
+{
+    public enum StorageOption
+    {
+        FILE_SYSTEM,
+        CLOUD,
+        HTTP
+    }
+    
+    public enum ConvertOption
+    {
+        TO_JSON,
+        TO_XML,
+        TO_YAML
+    }
+}
