c6bb47963943447c0d23ea5f164861759dfd3f24
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-06-04 18:37:14 +0300

    feat: add IFileWriter interface

---
+++ Backend_Homework/Abstract/IFileWriter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend_Homework.Abstract
+{
+    internal interface IFileWriter
+    {
+        void WriteFile(string fileName, string context);
+    }
+}
