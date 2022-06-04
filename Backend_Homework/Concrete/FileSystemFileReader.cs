cfac6548d79a30ae62403b306043976f94f74f22
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-06-04 18:40:47 +0300

    feat: add FileSystemFileReader class and inherit interfaces, fill ReadFile method

---
+++ Backend_Homework/Concrete/FileSystemFileReader.cs
@@ -0,0 +1,26 @@
+﻿using Backend_Homework.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend_Homework.Concrete
+{
+    internal class FileSystemFileReader : IFileReader
+    {
+        public string ReadFile(string sourceFileName)
+        {
+            try
+            {
+                using FileStream sourceStream = File.Open(sourceFileName, FileMode.Open);
+                using StreamReader streamReader = new(sourceStream);
+                return streamReader.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An error occured while reading the file", ex);
+            }
+        }
+    }
+}
