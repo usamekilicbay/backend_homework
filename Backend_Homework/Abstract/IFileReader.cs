19cd6d625c4ecbfc412e6ee4fdcb2c7426d6c23a
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-06-06 14:22:11 +0300

    refactor: add sourcePath parameter to ReadFile method sign in IFileReader

--- Backend_Homework/Abstract/IFileReader.cs
+++ Backend_Homework/Abstract/IFileReader.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Backend_Homework.Abstract
 {
     public interface IFileReader
     {
-        string ReadFile();
+        string ReadFile(string sourcePath);
         string GetSourcePath();
     }
 }
