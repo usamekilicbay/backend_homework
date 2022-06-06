101388e9a17c657ed7e32ce29878c5e58a2a8484
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-06-06 23:29:55 +0300

    refactor: remove xml convert codes from GetConvertedFile method and implement GetDocument method to JsonConverter

--- Backend_Homework/Concrete/JsonConverter.cs
+++ Backend_Homework/Concrete/JsonConverter.cs
@@ -1,33 +1,32 @@
 ﻿using Backend_Homework.Abstract;
 using Continero.Homework;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 
 namespace Backend_Homework.Concrete
 {
     internal class JsonConverter : IFileConverter
     {
-        public string GetConvertedFile(string fileContext)
+        public string GetConvertedFile(Document document)
         {
             try
             {
-                XDocument xdoc = XDocument.Parse(fileContext);
-                Document doc = new()
-                {
-                    Title = xdoc.Root.Element("title").Value,
-                    Text = xdoc.Root.Element("text").Value
-                };
-
-                return JsonConvert.SerializeObject(doc);
+                return JsonConvert.SerializeObject(document);
             }
             catch (Exception ex)
             {
                 throw new Exception("An error occured while converting the file", ex);
+            }
+        }
+
+        public Document GetDocument(string fileContent)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<Document>(fileContent);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"An error occured while desealization from JSON", ex.Message);
             }
         }
     }
