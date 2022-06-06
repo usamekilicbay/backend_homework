715ce51efa17226b8d99cdaa7b5d6ffaf1904f41
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-06-06 23:26:48 +0300

    refactor: update parameter type of GetConvertedFile and add GetDocument method sign to IFileConverter

--- Backend_Homework/Abstract/IFileConverter.cs
+++ Backend_Homework/Abstract/IFileConverter.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Continero.Homework;
 
 namespace Backend_Homework.Abstract
 {
     public interface IFileConverter
     {
-        string GetConvertedFile(string context);
+        string GetConvertedFile(Document document);
+        Document GetDocument(string fileContent);
     }
 }
