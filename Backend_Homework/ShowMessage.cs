975951de146d2196061725d4fcef399e05549b63
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-06-06 10:23:43 +0300

    feat: ShowMessage class

---
+++ Backend_Homework/ShowMessage.cs
@@ -0,0 +1,29 @@
+﻿namespace Backend_Homework
+{
+    internal static class ShowMessage
+    {
+        public static void AskForWriteStorage()
+        {
+            Console.WriteLine("Please chose storage to write file");
+            Console.WriteLine($"{(int)StorageOption.FILE_SYSTEM} = {StorageOption.FILE_SYSTEM}");
+            Console.WriteLine($"{(int)StorageOption.CLOUD} = {StorageOption.CLOUD}");
+            Console.WriteLine($"{(int)StorageOption.HTTP} = {StorageOption.HTTP}");
+        }
+
+        public static void AskForConvertType()
+        {
+            Console.WriteLine("Please chose format to convert file");
+            Console.WriteLine($"{(int)ConvertOption.TO_JSON} = {ConvertOption.TO_JSON}");
+            Console.WriteLine($"{(int)ConvertOption.TO_XML} = {ConvertOption.TO_XML}");
+            Console.WriteLine($"{(int)ConvertOption.TO_YAML} = {ConvertOption.TO_YAML}");
+        }
+
+        public static void AskForReadStorage()
+        {
+            Console.WriteLine("Please chose storage to read file from");
+            Console.WriteLine($"{(int)StorageOption.FILE_SYSTEM} = {StorageOption.FILE_SYSTEM}");
+            Console.WriteLine($"{(int)StorageOption.CLOUD} = {StorageOption.CLOUD}");
+            Console.WriteLine($"{(int)StorageOption.HTTP} = {StorageOption.HTTP}");
+        }
+    }
+}
