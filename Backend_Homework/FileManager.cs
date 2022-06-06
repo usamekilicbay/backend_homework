191c19b123b4cb6f26871ae301e066de63cbd7be
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-06-06 10:28:50 +0300

    refactor!: remove inputs from RunConvert method in FileManager

--- Backend_Homework/FileManager.cs
+++ Backend_Homework/FileManager.cs
@@ -13,29 +13,18 @@ namespace Backend_Homework
             RunConvert();
         }
 
-        const string SOURCE_FILE = "Source Files";
-        const string TARGET_FILE = "Target Files";
-
         private readonly IFileReader _fileReader;
         private readonly IFileConverter _fileConverter;
         private readonly IFileWriter _fileWriter;
 
         private void RunConvert()
         {
-            Console.WriteLine("Enter the name of the file you want to upload (with extension)");
-            string sourceFileName = Console.ReadLine();
-
-            Console.WriteLine("Enter a name for the new file (with extension)");
-            string targetFileName = Console.ReadLine();
-
-            string sourcePath = Path.Combine(Directory.GetCurrentDirectory(), $"{SOURCE_FILE}\\{sourceFileName}");
-            string targetPath = Path.Combine(Directory.GetCurrentDirectory(), $"{TARGET_FILE}\\{targetFileName}");
-
             try
             {
-                string fileContext = _fileReader.ReadFile(sourcePath);
+                string fileContext = _fileReader.ReadFile();
                 string serializedDoc = _fileConverter.GetConvertedFile(fileContext);
-                _fileWriter.WriteFile(targetPath, serializedDoc);
+                _fileWriter.WriteFile(serializedDoc);
+                Console.WriteLine("Operation successful!");
             }
             catch (Exception ex)
             {
@@ -43,7 +32,6 @@ namespace Backend_Homework
                 Console.WriteLine(ex.InnerException);
             }
 
-            Console.WriteLine("Operation successful!");
             Console.WriteLine("Press a key to quit...");
 
             Console.ReadKey();
