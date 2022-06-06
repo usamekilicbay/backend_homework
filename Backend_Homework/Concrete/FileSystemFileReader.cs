5c23eb5ecafe0258cc5e4aca7c9690cf0f77d179
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-06-06 10:27:46 +0300

    refactor!: implement GetSourcePath method and write algorithm for looping till get correct file name from user in FileSystemFileReader

--- Backend_Homework/Concrete/FileSystemFileReader.cs
+++ Backend_Homework/Concrete/FileSystemFileReader.cs
@@ -9,11 +9,13 @@ namespace Backend_Homework.Concrete
 {
     internal class FileSystemFileReader : IFileReader
     {
-        public string ReadFile(string sourceFileName)
+        const string SOURCE_FOLDER = "Source Files";
+
+        public string ReadFile()
         {
             try
             {
-                using FileStream sourceStream = File.Open(sourceFileName, FileMode.Open);
+                using FileStream sourceStream = File.Open(GetSourcePath(), FileMode.Open);
                 using StreamReader streamReader = new(sourceStream);
                 return streamReader.ReadToEnd();
             }
@@ -21,6 +23,38 @@ namespace Backend_Homework.Concrete
             {
                 throw new Exception("An error occured while reading the file", ex);
             }
+        }
+
+        public string GetSourcePath()
+        {
+            bool isFound = false;
+            string sourcePath = string.Empty;
+            do
+            {
+                Console.WriteLine("Enter the name of the file you want to upload (with extension)");
+                string sourceFileName = Console.ReadLine();
+                Console.WriteLine("###################");
+
+                if (string.IsNullOrEmpty(sourceFileName))
+                {
+                    Console.WriteLine("File not found, try again...");
+                    Console.WriteLine("###################");
+                    continue;
+                }
+
+                sourcePath = Path.Combine(Directory.GetCurrentDirectory(), SOURCE_FOLDER, sourceFileName);
+
+                isFound = File.Exists(sourcePath);
+
+                if (!isFound)
+                {
+                    Console.WriteLine($"{sourcePath} | File not found, try again...");
+                    Console.WriteLine("###################");
+                }
+
+            } while (!isFound);
+           
+            return sourcePath;
         }
     }
 }
