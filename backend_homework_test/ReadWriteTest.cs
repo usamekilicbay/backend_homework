d9c54f0deced1dcf04ca570b3c63afbdb4b0f71e
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-06-06 14:36:35 +0300

    feat: implement FileRead_ValidPathShouldWork unit test to ReadSystemTest

--- backend_homework_test/ReadWriteTest.cs
+++ backend_homework_test/ReadWriteTest.cs
@@ -1,12 +1,18 @@
 using Backend_Homework.Abstract;
+using Backend_Homework.Concrete;
 
 namespace backend_homework_test
 {
-    public class ReadWriteTest
+    public class FileSystemTest
     {
         [Fact]
-        public void FileReadShouldWork()
+        public void FileRead_ValidPathShouldWork()
         {
+            IFileReader fileReader = new FileSystemFileReader();
+            
+            string actual = fileReader.ReadFile("C:\\Users\\usame\\source\\repos\\backend_homework\\Backend_Homework\\Source Files\\Document1.xml");
+
+            Assert.True(actual.Length > 0);
         }
     }
 }
