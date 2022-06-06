c149ba353ea49d0f47df181b2fe1d0d825d0a942
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-06-06 23:31:07 +0300

    feat: implement method signs from IFileConverter and write logic for converting from string to xml and xml to object in XmlConverter

---
+++ Backend_Homework/Concrete/XmlConverter.cs
@@ -0,0 +1,29 @@
+﻿using Backend_Homework.Abstract;
+using Continero.Homework;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace Backend_Homework.Concrete
+{
+    internal class XmlConverter : IFileConverter
+    {
+        public Document GetDocument(string fileContent)
+        {
+            XDocument xdoc = XDocument.Parse(fileContent);
+            return new Document()
+            {
+                Title = xdoc.Root.Element("title").Value,
+                Text = xdoc.Root.Element("text").Value
+            };
+        }
+
+        public string GetConvertedFile(Document document)
+        {
+            using StringWriter stringWriter = new();
+            XmlSerializer xmlSerializer = new(typeof(Document));
+            xmlSerializer.Serialize(stringWriter, document);
+            return stringWriter.ToString();
+        }
+    }
+}
