7a66b25e45456d34f1e93d7f2937f99b9a2e9fce
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-06-06 10:26:00 +0300

    feat: add JsonConverter and inherit IFileConverter

---
+++ Backend_Homework/Concrete/JsonConverter.cs
@@ -0,0 +1,34 @@
+﻿using Backend_Homework.Abstract;
+using Continero.Homework;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Backend_Homework.Concrete
+{
+    internal class JsonConverter : IFileConverter
+    {
+        public string GetConvertedFile(string fileContext)
+        {
+            try
+            {
+                XDocument xdoc = XDocument.Parse(fileContext);
+                Document doc = new()
+                {
+                    Title = xdoc.Root.Element("title").Value,
+                    Text = xdoc.Root.Element("text").Value
+                };
+
+                return JsonConvert.SerializeObject(doc);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An error occured while converting the file", ex);
+            }
+        }
+    }
+}
