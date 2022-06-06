025e96a5aa8c9a65db9ee8b9cb36a3c7f38bb041
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-06-06 23:43:40 +0300

    refactor!!: implement _fileConverterTo and _fileConverterFrom properties, add them to constructor update logic of convert calling in FileManager

--- Backend_Homework/FileManager.cs
+++ Backend_Homework/FileManager.cs
@@ -1,20 +1,23 @@
 ﻿using Backend_Homework.Abstract;
+using Continero.Homework;
 
 namespace Backend_Homework
 {
     internal class FileManager
     {
-        public FileManager(IFileReader fileReader, IFileConverter fileConverter, IFileWriter fileWriter)
+        public FileManager(IFileReader fileReader, IFileConverter fileConverterFrom, IFileConverter fileConverterTo, IFileWriter fileWriter)
         {
             _fileReader = fileReader;
-            _fileConverter = fileConverter;
+            _fileConverterFrom = fileConverterFrom;
+            _fileConverterTo = fileConverterTo;
             _fileWriter = fileWriter;
 
             RunConvert();
         }
 
         private readonly IFileReader _fileReader;
-        private readonly IFileConverter _fileConverter;
+        private readonly IFileConverter _fileConverterFrom;
+        private readonly IFileConverter _fileConverterTo;
         private readonly IFileWriter _fileWriter;
 
         private void RunConvert()
@@ -22,7 +25,8 @@ namespace Backend_Homework
             try
             {
                 string fileContext = _fileReader.ReadFile(_fileReader.GetSourcePath());
-                string serializedDoc = _fileConverter.GetConvertedFile(fileContext);
+                Document document = _fileConverterFrom.GetDocument(fileContext);
+                string serializedDoc = _fileConverterTo.GetConvertedFile(document);
 
                 _fileWriter.WriteFile(_fileWriter.GetTargetPath(_fileWriter.GetTargetFileName()), serializedDoc);
                 Console.WriteLine("Operation successful!");
