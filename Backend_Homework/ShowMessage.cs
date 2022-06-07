dce73a106b3a132b387003ed94f1d03ed7097e2d
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-06-07 10:43:54 +0300

    refactor: update AskFor methods, add parameter and mutliple user message options

--- Backend_Homework/ShowMessage.cs
+++ Backend_Homework/ShowMessage.cs
@@ -2,17 +2,24 @@
 {
     internal static class ShowMessage
     {
-        public static void AskForStorage(string operation)
+        public static void AskForStorage(OperationType operationType)
         {
-            Console.WriteLine($"Please chose storage for file {operation} operation");
+            Console.WriteLine($"Please chose storage for file {operationType == OperationType.READ: \"read\" : \"write\"} operation");
             Console.WriteLine($"{(int)StorageOption.FILE_SYSTEM} = {StorageOption.FILE_SYSTEM}");
             Console.WriteLine($"{(int)StorageOption.CLOUD} = {StorageOption.CLOUD}");
             Console.WriteLine($"{(int)StorageOption.HTTP} = {StorageOption.HTTP}");
         }
 
-        public static void AskForConvertType()
+        public static void AskForConvertType(OperationType operationType)
         {
-            Console.WriteLine("Please chose format to convert file");
+            string message = operationType switch
+            {
+                OperationType.READ => "Please chose upload file format",
+                OperationType.WRITE => "Please chose format to convert uploaded file",
+                _ => "Please chose upload file format",
+            };
+
+            Console.WriteLine(message);
             Console.WriteLine($"{(int)ConvertOption.JSON} = {ConvertOption.JSON}");
             Console.WriteLine($"{(int)ConvertOption.XML} = {ConvertOption.XML}");
             Console.WriteLine($"{(int)ConvertOption.YAML} = {ConvertOption.YAML}");
