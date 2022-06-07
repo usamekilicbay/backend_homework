f65e855078dd474603d2f73756e57ec2478fcfc9
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-06-07 10:33:18 +0300

    refactor: simplifying AskFor methods in ShowMessage

--- Backend_Homework/ShowMessage.cs
+++ Backend_Homework/ShowMessage.cs
@@ -2,9 +2,9 @@
 {
     internal static class ShowMessage
     {
-        public static void AskForWriteStorage()
+        public static void AskForStorage(string operation)
         {
-            Console.WriteLine("Please chose storage to write file");
+            Console.WriteLine($"Please chose storage for file {operation} operation");
             Console.WriteLine($"{(int)StorageOption.FILE_SYSTEM} = {StorageOption.FILE_SYSTEM}");
             Console.WriteLine($"{(int)StorageOption.CLOUD} = {StorageOption.CLOUD}");
             Console.WriteLine($"{(int)StorageOption.HTTP} = {StorageOption.HTTP}");
@@ -13,17 +13,9 @@
         public static void AskForConvertType()
         {
             Console.WriteLine("Please chose format to convert file");
-            Console.WriteLine($"{(int)ConvertOption.TO_JSON} = {ConvertOption.TO_JSON}");
-            Console.WriteLine($"{(int)ConvertOption.TO_XML} = {ConvertOption.TO_XML}");
-            Console.WriteLine($"{(int)ConvertOption.TO_YAML} = {ConvertOption.TO_YAML}");
-        }
-
-        public static void AskForReadStorage()
-        {
-            Console.WriteLine("Please chose storage to read file from");
-            Console.WriteLine($"{(int)StorageOption.FILE_SYSTEM} = {StorageOption.FILE_SYSTEM}");
-            Console.WriteLine($"{(int)StorageOption.CLOUD} = {StorageOption.CLOUD}");
-            Console.WriteLine($"{(int)StorageOption.HTTP} = {StorageOption.HTTP}");
+            Console.WriteLine($"{(int)ConvertOption.JSON} = {ConvertOption.JSON}");
+            Console.WriteLine($"{(int)ConvertOption.XML} = {ConvertOption.XML}");
+            Console.WriteLine($"{(int)ConvertOption.YAML} = {ConvertOption.YAML}");
         }
     }
 }
